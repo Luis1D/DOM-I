cddf44e154a4b448587161045d436fd7b787fba5
Luis De Anda <luisdeanda916@outlook.com>
2019-06-17 15:26:55 -0700

    MVP Finished

--- js/index.js
+++ js/index.js
@@ -40,3 +40,83 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Navigation
+const nav = document.querySelector('nav');
+const navItems = document.querySelectorAll('a');
+navItems[0].textContent = siteContent["nav"]["nav-item-1"];
+navItems[0].style.color = "green";
+navItems[1].textContent = siteContent["nav"]["nav-item-2"];
+navItems[1].style.color = "green";
+navItems[2].textContent = siteContent["nav"]["nav-item-3"];
+navItems[2].style.color = "green";
+navItems[3].textContent = siteContent["nav"]["nav-item-4"];
+navItems[3].style.color = "green";
+navItems[4].textContent = siteContent["nav"]["nav-item-5"];
+navItems[4].style.color = "green";
+navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+navItems[5].style.color = "green";
+
+const lastItem = document.createElement('a');
+const firstItem = document.createElement('a');
+lastItem.textContent = "Last Item";
+firstItem.textContent = "First Item";
+nav.appendChild(lastItem);
+nav.prepend(firstItem);
+
+// Call To Action 
+const cta = document.querySelector('.cta');
+const ctaHeader = document.querySelector('h1');
+const ctaImg = document.getElementById('cta-img');
+const button = document.querySelector('button');
+
+ctaHeader.textContent = siteContent['cta']['h1'];
+const br = document.createElement('br');
+
+
+
+button.textContent = siteContent['cta']['button'];
+ctaImg.setAttribute('src', siteContent['cta']["img-src"]);
+
+// MAIN CONTENT
+const mainContent = document.querySelector('.main-content');
+const mcH4 = document.querySelectorAll('.text-content > h4');
+const mcPara = document.querySelectorAll('.text-content > p');
+
+//FEATURES
+mcH4[0].textContent = siteContent['main-content']['features-h4'];
+mcPara[0].textContent = siteContent['main-content']['features-content'];
+
+//ABOUT
+mcH4[1].textContent = siteContent['main-content']['about-h4'];
+mcPara[1].textContent = siteContent['main-content']['about-content'];
+
+//MIDDLE IMG
+const middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//SERVICES
+mcH4[2].textContent = siteContent['main-content']['services-h4'];
+mcPara[2].textContent = siteContent['main-content']['services-content'];
+
+//PRODUCT
+mcH4[3].textContent = siteContent['main-content']['product-h4'];
+mcPara[3].textContent = siteContent['main-content']['product-content'];
+
+//VISION
+mcH4[4].textContent = siteContent['main-content']['vision-h4'];
+mcPara[4].textContent = siteContent['main-content']['vision-content'];
+
+//CONTACT
+const contactHeader = document.querySelector('.contact > h4');
+const contactPara = document.querySelectorAll('.contact > p');
+
+contactHeader.textContent = siteContent['contact']['contact-h4'];
+contactPara[0].textContent = siteContent['contact']['address'];
+contactPara[1].textContent = siteContent['contact']['phone'];
+contactPara[2].textContent = siteContent['contact']['email'];
+
+//FOOTER 
+const footerTxt = document.querySelector('footer > p');
+
+footerTxt.textContent = siteContent['footer']['copyright'];
